9a63b60b094083333d7fdf7bc286003f5892d822
Brian Dear <superacidjax@me.com>
2017-03-28 15:30:39 +0200

    initial commit

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -2,10 +2,14 @@ import Vapor
 
 let drop = Droplet()
 
+drop.get("hello") { request in
+      return "Hello, world!"
+}
+
 drop.get { req in
-    return try drop.view.make("welcome", [
-    	"message": drop.localization[req.lang, "welcome", "title"]
-    ])
+  return try drop.view.make("welcome", [
+    "message": drop.localization[req.lang, "welcome", "title"]
+  ])
 }
 
 drop.resource("posts", PostController())
